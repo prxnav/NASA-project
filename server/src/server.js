6649a42f8255ec38130cce09d3db358d51f5ec99
Pranav Menon <ppmenon02@gmail.com>
2023-06-16 17:15:23 +0530

    fetched planets to frontend

--- server/src/server.js
+++ server/src/server.js
@@ -2,10 +2,17 @@ const app = require("./app");
 
 const http = require("http");
 
+const { loadPlanetsData } = require("./models/planets.model");
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Listening on ${PORT}...`);
-});
+async function startServer() {
+  await loadPlanetsData();
+  server.listen(PORT, () => {
+    console.log(`Listening on ${PORT}...`);
+  });
+}
+
+startServer();
