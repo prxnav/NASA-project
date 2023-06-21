c8005e4273a1a155a94c3474b17980c226e17c09
Pranav Menon <ppmenon02@gmail.com>
2023-06-22 00:10:32 +0530

    script + morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
+const morgan = require("morgan");
 const planetsRouter = require("./routes/planets/planets.router");
 const app = express();
 
 app.use(
   cors({
-    orign: "http://localhost:3000",
+    origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
 module.exports = app;
