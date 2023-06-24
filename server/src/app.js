4f5b4f0932a84a01628ee2f230343f76a3fe7c7f
Pranav Menon <ppmenon02@gmail.com>
2023-06-24 10:30:22 +0530

    func restruct

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const morgan = require("morgan");
-const planetsRouter = require("./routes/planets/planets.router");
 const app = express();
+
+const planetsRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router");
 
 app.use(
   cors({
@@ -11,11 +13,14 @@ app.use(
   })
 );
 app.use(morgan("combined"));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
-app.use(planetsRouter);
 
-app.get("/", (req, res) => {
+app.use(planetsRouter);
+app.use(launchesRouter);
+
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
